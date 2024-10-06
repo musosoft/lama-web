4f3d4a5e89929418c801aa0ec0fd4309cdcd9f58
Roman Kovac <musosoft@gmail.com>
2024-10-07 02:56:19 +0700

    Update url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()]
+  site: 'https://musosoft.github.io',
+  base: 'lama-web',
 });
