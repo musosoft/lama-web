8d240c5148a584b19347d03d2765659e83f94efa
Roman Kovac <musosoft@gmail.com>
2024-10-07 03:37:59 +0700

    Fix typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
   site: 'https://musosoft.github.io',
   base: 'lama-web',
 });
