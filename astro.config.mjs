09e35fc991ca64e42938d8769ffe92d22d05af4f
Roman Kovac <musosoft@gmail.com>
2024-10-21 03:13:32 +0700

    Fix adapter files moving

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  base: 'lama-web',
+  base: '/lama-web',
   site: 'https://musosoft.github.io',
   output: 'server',
 
