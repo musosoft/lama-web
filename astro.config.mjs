4e0bc7d529bdfbb5be23958aa39db228a5ab3261
Roman Kovac <musosoft@gmail.com>
2024-10-21 03:30:39 +0700

    Test UA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   base: '/lama-web',
   site: 'https://musosoft.github.io',
-  output: 'hybrid',
 
   integrations: [
     tailwind(),
   ],
-
-  adapter: cloudflare({
-    imageService: 'cloudflare'
-  }),
 });
