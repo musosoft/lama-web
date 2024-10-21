235e7b123d3734641718dafa54c5bed306e9113a
Roman Kovac <musosoft@gmail.com>
2024-10-22 03:33:32 +0700

    Test Shoutbox

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/lama-web',
-  site: 'https://musosoft.github.io',
+  site: 'https://lama-web.pages.dev',
 
   vite: {
     define: {
