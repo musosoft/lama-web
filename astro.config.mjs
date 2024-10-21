2817a0f717626dbe28c7260c6efb4030fddbadf6
Roman Kovac <musosoft@gmail.com>
2024-10-21 23:38:02 +0700

    migrate to Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   base: '/lama-web',
   site: 'https://musosoft.github.io',
+
   vite: {
     define: {
-      'process.env.SECRET': JSON.stringify(process.env.SECRET)
-    }
+      'process.env': process.env,
+    },
   },
-  integrations: [
-    tailwind(),
-  ],
+
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: cloudflare(),
 });
